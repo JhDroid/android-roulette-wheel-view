7ef0aea1f7091ecae1007d8e537a85521fec85eb
JhDroid <lorzx77@gmail.com>
2021-02-19 16:39:15 +0900

    룰렛 테스트 코드 추가

--- app/src/main/java/com/jhdroid/roulette/MainActivity.kt
+++ app/src/main/java/com/jhdroid/roulette/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.jhdroid.roulette
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.jhdroid.roulette.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val rouletteData = listOf("HELLO!", "HELLO@")
+
+        binding.roulette.apply {
+            setRouletteSize(2)
+            setRouletteDataList(rouletteData)
+        }
+
+        binding.rotateBtn.setOnClickListener {
+            val toDegrees = (2000..10000).random().toFloat()
+            binding.roulette.rotateRoulette(toDegrees, 4000)
+        }
     }
 }
