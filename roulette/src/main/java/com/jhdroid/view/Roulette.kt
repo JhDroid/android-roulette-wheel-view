fbdc7c3eb4c40aa1fac7f699fbea7707aca35b17
JhDroid <lorzx77@gmail.com>
2021-07-07 23:38:42 +0900

    rxjava import 제거

--- roulette/src/main/java/com/jhdroid/view/Roulette.kt
+++ roulette/src/main/java/com/jhdroid/view/Roulette.kt
@@ -7,7 +7,6 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
-import io.reactivex.Single
 import kotlin.math.cos
 import kotlin.math.sin
 
